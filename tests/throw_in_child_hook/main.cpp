d0e4b495a512b2896d27379259c9a9498d441a98
Yauheni Akhotnikau <eao197@gmail.com>
2020-02-27 16:12:23 +0300

    Additional test cases.

--- tests/throw_in_child_hook/main.cpp
+++ tests/throw_in_child_hook/main.cpp
@@ -31,6 +31,59 @@ TEST_CASE("Exception in child-hook")
 	REQUIRE(child.exited());
 }
 
+TEST_CASE("Exception in child-hook (long description)")
+{
+	procxx::process child{child_name};
+
+	REQUIRE_THROWS_WITH(
+		child.exec([](procxx::process::hook_place where) {
+				if(procxx::process::hook_place::child == where)
+				{
+					throw std::runtime_error(
+							"_123456789_123456789_123456789_123456789"
+							"_123456789_123456789_123456789_123456789"
+							"_123456789_123456789_123456789_123456789"
+							"_123456789_123456789_123456789_123456789");
+				}
+			})
+		,
+		"exception in child: "
+		"_123456789_123456789_123456789_123456789"
+		"_123456789_123456789_123456789_123456789"
+		"_123456789_123456789_123456789_123456789"
+		"_123456"
+	);
+
+	REQUIRE(!child.waited());
+	REQUIRE_NOTHROW(child.wait());
+
+	REQUIRE(!child.running());
+	REQUIRE(child.exited());
+}
+
+
+TEST_CASE("Non-standard exception in child-hook")
+{
+	procxx::process child{child_name};
+
+	REQUIRE_THROWS_WITH(
+		child.exec([](procxx::process::hook_place where) {
+				if(procxx::process::hook_place::child == where)
+				{
+					throw 42;
+				}
+			})
+		,
+		"unknown exception in child (description is not available)"
+	);
+
+	REQUIRE(!child.waited());
+	REQUIRE_NOTHROW(child.wait());
+
+	REQUIRE(!child.running());
+	REQUIRE(child.exited());
+}
+
 int main(int argc, char ** argv)
 {
 	(void)signal(SIGPIPE, SIG_IGN);
