b8c969410e6bee0286b1b3c03eb9ef62ace4d906
Yauheni Akhotnikau <eao197@gmail.com>
2020-03-06 09:19:33 +0300

    Switch to the fresh version of procyy.

--- externals.rb
+++ externals.rb
@@ -5,7 +5,7 @@ MxxRu::arch_externals :doctest do |e|
 end
 
 MxxRu::arch_externals :procxx do |e|
-  e.url 'https://github.com/eao197/procyy/archive/22c4b7a577d6feb88994e77a4910687d1a318925.tar.gz'
+  e.url 'https://github.com/eao197/procyy/archive/15c531dc0c7ce5e81c84a1535efe61a2fe89cf8a.tar.gz'
 
   e.map_file 'include/process.h' => 'procyy/*'
 end
